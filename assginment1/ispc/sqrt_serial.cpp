5936c96261e21ae5cdd5461a62734fd37d1a831f
MrHohn <zzhonzi@gmail.com>
2015-10-02 22:40:47 -0400

    Packaged two functions into one for serial

--- assginment1/ispc/sqrt_serial.cpp
+++ assginment1/ispc/sqrt_serial.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 double absDouble(double num)
 {
@@ -9,50 +8,51 @@ double absDouble(double num)
 		return num; 
 }
 
-double sqrtDouble(double num)
-{
-	// if input num is 0, return 0
-	if (num == 0)
-		return 0;
-
-	/*
-		Setup the initial estimation, refer wikipedia's Rough estimation:
-		https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
-		
-		Suppose: 
-		sqrt(S) = sqrt(a) * 10^n
-		sqrt(S) = { 2 * 10^n, if a < 10
-		            6 * 10^n, if a >= 10 }
-
-		Since for this assignment, we always calculate the sqrt of random
-		number between 0 ~ 3, thus a < 10, and n = 0
-		Hence the initial value would be set as 2 * 10^0 = 2
-	*/
-
-	// setup the record for last generation and current generation value
-	double numLast = 2, numCurrent;
-	while (1)
-	{
-		// apply Newton's method to calculate the sqrt value
-		numCurrent = (numLast + (num / numLast)) / 2;
-		// judge if the accuracy is enough, 10^-4 = 0.0001
-		if (absDouble(numCurrent - numLast) <= 0.0001)
-		{
-			return numCurrent;
-		}
-		// put the current value as last value to prepare for next loop
-		numLast = numCurrent;
-	}
-}
-
 void sqrt_serial(int N, double* nums, double* result)
 {
 	// start looping and calculating all the sqrt result
 	int i;
 	for (i = 0; i < N; ++i)
 	{
-	    result[i] = sqrtDouble(nums[i]);
-	    // printf("result is: %f, loop count is: %d\n", result[i], count);
+		// if input num is 0, return 0
+		if (nums[i] == 0)
+		{
+			result[i] = 0;
+			continue;
+		}
+
+		/*
+			Setup the initial estimation, refer wikipedia's Rough estimation:
+			https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
+			
+			Suppose: 
+			sqrt(S) = sqrt(a) * 10^n
+			sqrt(S) = { 2 * 10^n, if a < 10
+			            6 * 10^n, if a >= 10 }
+
+			Since for this assignment, we always calculate the sqrt of random
+			number between 0 ~ 3, thus a < 10, and n = 0
+			Hence the initial value would be set as 2 * 10^0 = 2
+		*/
+
+		// setup the record for last generation and current generation value
+		double numLast = 2, numCurrent;
+		while (1)
+		{
+			// apply Newton's method to calculate the sqrt value
+			numCurrent = (numLast + (nums[i] / numLast)) / 2;
+			// judge if the accuracy is enough, 10^-4 = 0.0001
+			if (absDouble(numCurrent - numLast) <= 0.0001)
+			{
+				result[i] = numCurrent;
+			    // printf("result is: %f\n", result[i]);
+				break;
+			}
+			// put the current value as last value to prepare for next loop
+			numLast = numCurrent;
+		}
+
+
 	}
 }
 
