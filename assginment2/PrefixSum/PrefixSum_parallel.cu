321bffe79f38d9c1afaed9703a2af27cf683aa46
MrHohn <zzhonzi@gmail.com>
2015-11-04 11:56:51 -0500

    Changed some signatures of methods

--- assginment2/PrefixSum/PrefixSum_parallel.cu
+++ assginment2/PrefixSum/PrefixSum_parallel.cu
@@ -7,92 +7,84 @@
 #include "timing.h"
 #include "util.h"
 
-__global__ void upSweep(int* device_result, int length, int twod, int twod1) {
-    int index = (blockIdx.x * blockDim.x + threadIdx.x) * twod1;
-    if ((index + twod1 - 1) >= length) {  // check boundary
-        return;
-    }
-    device_result[index + twod1 - 1] += device_result[index + twod - 1];
-}
-
-__global__ void downSweep(int* device_result, int length, int twod, int twod1) {
-    int index = (blockIdx.x * blockDim.x + threadIdx.x) * twod1;
-    if ((index + twod1 - 1) >= length) {  // check boudnary
-        return;
-    }
-    int tmp = device_result[index + twod - 1];
-    device_result[index + twod - 1] = device_result[index + twod1 - 1];
-    device_result[index + twod1 - 1] += tmp;
-}
-
-void exclusive_scan_parallel(int* nums, int len, int* output, double& time_cost)
+// this method is cited from CMU's assignment description
+// http://15418.courses.cs.cmu.edu/spring2015/article/4
+__global__ void buildUpTree(int* device_result, int len, int starter, int interval)
 {
-    int* device_result;
-    int* device_input;
+    int index = (blockIdx.x * blockDim.x + threadIdx.x) * interval;
+    // return if out of range
+    if (index + interval > len)
+    {  
+        return;
+    }
+    device_result[index + interval - 1] += device_result[index + starter - 1];
+}
 
-    int roundedLen = roundPowerTwo(len);
+__global__ void buildDownTree(int* device_result, int len, int starter, int interval)
+{
+    int index = (blockIdx.x * blockDim.x + threadIdx.x) * interval;
+    // return if out of range
+    if (index + interval > len)
+    {  
+        return;
+    }
+    int temp = device_result[index + starter - 1];
+    device_result[index + starter - 1] = device_result[index + interval - 1];
+    device_result[index + interval - 1] += temp;
+}
 
-    // allocate space on GPU and copy inputs into it
-    cudaMalloc((void **)&device_result, roundedLen * sizeof(int));
-    cudaMalloc((void **)&device_input, roundedLen * sizeof(int));
-    cudaMemcpy(device_input, nums, len * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_result, nums, len * sizeof(int), cudaMemcpyHostToDevice);
-
-    // start to do GPU computing
-    reset_and_start_timer();
-
+void launch_scan(int roundedLen, int* device_result)
+{
     int threadsPerBlock = 256;
     
-    // upsweep phase
-    for (int twod = 1; twod < roundedLen; twod *= 2) {
-        int twod1 = twod * 2;
-        int blocksPerGrid = (roundedLen/twod1 + threadsPerBlock - 1) / threadsPerBlock;
-        upSweep<<<blocksPerGrid, threadsPerBlock>>>(device_result, roundedLen, twod, twod1);
+    // build up tree
+    for (int starter = 1; starter < roundedLen; starter *= 2)
+    {
+        int interval = starter * 2;
+        // plus (threadsPerBlock - 1) is actually ceiling the block number
+        int numBlocks = (roundedLen / interval + threadsPerBlock - 1) / threadsPerBlock;
+        buildUpTree<<<numBlocks, threadsPerBlock>>>(device_result, roundedLen, starter, interval);
     }
 
     // set last element to zero
     int zero = 0;
     cudaMemcpy(&device_result[roundedLen - 1], &zero, sizeof(int), cudaMemcpyHostToDevice);
 
-    // downsweep phase
-    for (int twod = roundedLen / 2; twod >= 1; twod /= 2) {
-        int twod1 = twod * 2;
-        int blocksPerGrid = (roundedLen/twod1 + threadsPerBlock - 1) / threadsPerBlock;
-        downSweep<<<blocksPerGrid, threadsPerBlock>>>(device_result, roundedLen, twod, twod1);
+    // build down tree
+    for (int starter = roundedLen / 2; starter >= 1; starter /= 2)
+    {
+        int interval = starter * 2;
+        // plus (threadsPerBlock - 1) is actually ceiling the block number
+        int numBlocks = (roundedLen / interval + threadsPerBlock - 1) / threadsPerBlock;
+        buildDownTree<<<numBlocks, threadsPerBlock>>>(device_result, roundedLen, starter, interval);
     }
-
-    // Wait for any work left over to be completed.
-    cudaThreadSynchronize();
-
-    // finished GPU computing
-    time_cost = get_elapsed_mcycles();
-    
-    // copy back the result
-    cudaMemcpy(output, device_result, len * sizeof(int), cudaMemcpyDeviceToHost);
-    // free device memory
-    cudaFree(device_result);
-    cudaFree(device_input);
 }
 
-void printCudaInfo()
+
+void exclusive_scan_parallel(int* nums, int len, int* output, double& time_cost)
 {
-    // for fun, just print out some stats on the machine
+    int* device_result;
 
-    int deviceCount = 0;
-    cudaError_t err = cudaGetDeviceCount(&deviceCount);
+    int roundedLen = roundPowerTwo(len);
 
-    printf("---------------------------------------------------------\n");
-    printf("Found %d CUDA devices\n", deviceCount);
+    // Allocate space on GPU and copy inputs into it
+    cudaMalloc((void **)&device_result, roundedLen * sizeof(int));
+    cudaMemcpy(device_result, nums, len * sizeof(int), cudaMemcpyHostToDevice);
 
-    for (int i = 0; i < deviceCount; i++)
-    {
-        cudaDeviceProp deviceProps;
-        cudaGetDeviceProperties(&deviceProps, i);
-        printf("Device %d: %s\n", i, deviceProps.name);
-        printf("   SMs:        %d\n", deviceProps.multiProcessorCount);
-        printf("   Global mem: %.0f MB\n",
-               static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024));
-        printf("   CUDA Cap:   %d.%d\n", deviceProps.major, deviceProps.minor);
-    }
-    printf("---------------------------------------------------------\n"); 
+    // Start to do GPU computing
+    reset_and_start_timer();
+
+    // Since in-place algorithm are used, we did not allocate device_input
+    launch_scan(roundedLen, device_result);
+
+    // Wait for all instances to finished
+    cudaThreadSynchronize();
+
+    // Finished GPU computing
+    time_cost = get_elapsed_mcycles();
+    
+    // Copy back the result
+    cudaMemcpy(output, device_result, len * sizeof(int), cudaMemcpyDeviceToHost);
+    // Free device memory
+    cudaFree(device_result);
 }
