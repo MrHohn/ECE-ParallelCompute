cabd1f9d7d82a147878753196c9feab38e0351cd
MrHohn <zzhonzi@gmail.com>
2015-11-03 21:14:08 -0500

    Added codes for reading inputs from sample file, attached sampleinput.txt

--- assginment2/PrefixSum/main.cpp
+++ assginment2/PrefixSum/main.cpp
@@ -1,45 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "timing.h"
 #include "util.hpp"
 
 using namespace std;
 
 void exclusive_scan_serial(int* nums, int len, int* output);
-int find_repeats_serial(int *nums, int len, int *outputB, int* outputC);
+int find_repeats_serial(int* nums, int len, int* outputB, int* outputC);
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
     printf("\n----------------------- GPU TEST BEGIN ---------------------\n\n");
 
     static int test_iteration = 3;
-    int rMin = 0, rMax = 10000;
-    int N = 1 * 1000 * 1000;
-    // int N = 5;
-    // get the needed len by rounding the input array length to the next power of 2 to simplify the calculation
-    int len = roundPowerTwo(N);
-    // printf("The actual len is : %d\n\n", len);
-        
-    // first create enough random number in this range (0, 1,000,000)
-    int* input = new int[len];
-    int* outputB = new int[len];
-    int* outputC = new int[len];
-    
-    // initialize the input integers
-    createRand(input, len, rMin, rMax);
 
-    // input[0] = 1;
-    // input[1] = 1;
-    // input[2] = 2;
-    // input[3] = 2;
-    // input[4] = 3;
+    int N, len;
+    string line;
+    int* input;
+    if (argc == 1)
+    {
+        N = 1 * 1000 * 1000;
+        // get the needed len by rounding the input array length to the next power of 2 to simplify the calculation
+        len = roundPowerTwo(N);
+        // printf("The actual len is : %d\n\n", len);
+        
+        input = new int[len];
+        // first create enough random number in this range (0, 1,000,000)
+        // initialize the input integers
+        int rMin = 0, rMax = 10000;
+        createRand(input, len, rMin, rMax);
+    }
+    // read input file
+    else
+    {
+        string file_name(argv[1]);
+        ifstream sample(file_name);
+        if (sample.is_open())
+        {
+            std::getline(sample, line);
+            sample.close();
+            char* line_chars = new char[line.size()];
+            sprintf(line_chars, "%s", line.c_str());
+            char* token = strtok(line_chars, " ,");
+            // first argument is the length
+            N = atoi(token);
+
+            len = roundPowerTwo(N);
+            // now initialize the input numbers from file
+            input = new int[len];
+            for (int i = 0; i < len; ++i)
+            {
+                if (i < N)
+                {
+                    token = strtok(NULL, " ,");
+                    input[i] = atoi(token);
+                }
+                else
+                {
+                    input[i] = 0;                    
+                }
+            }
+        }
+        else
+        {
+            printf("File: [%s] Not Found!\n", file_name.c_str());
+            printf("Exit now...\n");
+            printf("\n-------------------------- TEST END --------------------------\n\n");
+            exit(0);
+        }
+    }
 
     // printf("input:\n");
     // for (int i = 0; i < N; ++i)
     // {
     //     printf("%d\n", input[i]);
     // }
+
+    int* outputB = new int[len];
+    int* outputC = new int[len];
+    
 
     printf("First run the [serial find repeats]...\n");
     double minSerialFind = 1e30;
